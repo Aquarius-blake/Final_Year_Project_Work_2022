8e1d4c693ea99fdad4d876ce1732fb771ad11b7b
Aquarius-blake <55807270+Aquarius-blake@users.noreply.github.com>
2022-08-15 23:06:03 +0000

    Mobile profile screen

---
+++ lib/Screens/Home/Mobilepages/Mlikedposts.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Likedposts extends StatefulWidget {
+  const Likedposts({Key? key}) : super(key: key);
+
+  @override
+  State<Likedposts> createState() => _LikedpostsState();
+}
+
+class _LikedpostsState extends State<Likedposts> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
