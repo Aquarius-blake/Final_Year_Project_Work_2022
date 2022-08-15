b3a24b3ba280be21bef1cc20124eb286fffe4201
Aquarius-blake <55807270+Aquarius-blake@users.noreply.github.com>
2022-08-15 23:01:05 +0000

    Mobile profile screen

---
+++ lib/Screens/Home/Mobilepages/Profile_posts.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class Profilepost extends StatefulWidget {
+  const Profilepost({Key? key}) : super(key: key);
+
+  @override
+  State<Profilepost> createState() => _ProfilepostState();
+}
+
+class _ProfilepostState extends State<Profilepost> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
