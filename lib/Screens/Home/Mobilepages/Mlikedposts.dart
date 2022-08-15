313e6405912d8f6e71a520dac50bbb694c18c735
Aquarius-blake <55807270+Aquarius-blake@users.noreply.github.com>
2022-08-15 23:15:58 +0000

    Mobile profile screen

--- lib/Screens/Home/Mobilepages/Mlikedposts.dart
+++ lib/Screens/Home/Mobilepages/Mlikedposts.dart
@@ -16,7 +16,7 @@ class _LikedpostsState extends State<Likedposts> {
   Widget build(BuildContext context) {
     return Scaffold(
       body:  StreamBuilder(
-        stream: FirebaseFirestore.instance.collection('Posts').where('author uid', isEqualTo: widget.snap['uid']).snapshots(),
+        stream: FirebaseFirestore.instance.collection('Posts').where('likes',arrayContains: widget.snap['uid']).snapshots(),
         builder: (context, AsyncSnapshot<QuerySnapshot<Map<String,dynamic>>>snapshot){
           if(snapshot.connectionState==ConnectionState.waiting){
             return Center(
