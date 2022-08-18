f9f0ab1d6f6d9729b50f6955ce4553d2752c1878
Aquarius-blake <55807270+Aquarius-blake@users.noreply.github.com>
2022-08-18 23:57:05 +0000

    Edit profile mobile

---
+++ lib/Screens/Platforms/WebSceens/Web_editProfile.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+
+class WedProfile extends StatefulWidget {
+  const WedProfile({Key? key}) : super(key: key);
+
+  @override
+  State<WedProfile> createState() => _WedProfileState();
+}
+
+class _WedProfileState extends State<WedProfile> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
+
+
+
+
+
+
+
+
+
