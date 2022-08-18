fc8d71ecc8ccdb3d0e2ea17ff0fd46034f632f14
Aquarius-blake <55807270+Aquarius-blake@users.noreply.github.com>
2022-08-18 00:19:35 +0000

    Web profile screen

--- lib/Screens/Platforms/WebSceens/WprofileScreen.dart
+++ lib/Screens/Platforms/WebSceens/WprofileScreen.dart
@@ -68,7 +68,8 @@ CircleAvatar(
 ),
                         Column(
                           children: [
-
+Row(),
+                            Row()
                           ],
                         )
                       ],
