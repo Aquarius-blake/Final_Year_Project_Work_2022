09b4ea992ae70aedcaa119d0c0e3d9802b70c8cc
Aquarius-blake <55807270+Aquarius-blake@users.noreply.github.com>
2022-08-19 23:53:35 +0000

    Edit profile mobile

--- lib/Screens/Home/Mobilepages/Meditprofile.dart
+++ lib/Screens/Home/Mobilepages/Meditprofile.dart
@@ -179,10 +179,10 @@ class _MedprofileState extends State<Medprofile> {
                               borderSide: const BorderSide(
                                   color: Colors.redAccent
                               ),
-                              borderRadius: BorderRadius.circular(100.0),
+                              borderRadius: BorderRadius.circular(10.0),
                             ),
                             enabledBorder: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(100.0),
+                              borderRadius: BorderRadius.circular(10.0),
 
                             ),
                           ),
