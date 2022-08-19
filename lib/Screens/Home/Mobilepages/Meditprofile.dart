f2a1903d870fc794f00a10eec48cbea382c4b972
Aquarius-blake <55807270+Aquarius-blake@users.noreply.github.com>
2022-08-19 23:22:22 +0000

    Edit profile mobile + web +Notification screen

--- lib/Screens/Home/Mobilepages/Meditprofile.dart
+++ lib/Screens/Home/Mobilepages/Meditprofile.dart
@@ -58,8 +58,20 @@ _selectimage(BuildContext context)async{
         );
       }
   );
-
 }
+
+
+@override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
