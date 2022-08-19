02adf501bf1ae4390b87dd4676610617e3199fe8
Aquarius-blake <55807270+Aquarius-blake@users.noreply.github.com>
2022-08-19 00:55:51 +0000

    Edit profile mobile + web

---
+++ lib/shared/Widgets/notification_card.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class NotifCard extends StatefulWidget {
+  const NotifCard({Key? key}) : super(key: key);
+
+  @override
+  State<NotifCard> createState() => _NotifCardState();
+}
+
+class _NotifCardState extends State<NotifCard> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
