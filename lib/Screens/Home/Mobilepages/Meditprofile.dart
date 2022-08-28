2ecc08c5121590557e9b076ebec4b81a1c3193c3
Aquarius-blake <55807270+Aquarius-blake@users.noreply.github.com>
2022-08-28 07:22:46 +0000

    Edit profile mobile

--- lib/Screens/Home/Mobilepages/Meditprofile.dart
+++ lib/Screens/Home/Mobilepages/Meditprofile.dart
@@ -293,8 +293,8 @@ late DateTime _dateTime;
                               onPressed: (){},
                               child: Padding(
                                 padding: const EdgeInsets.only(
-                                    left:16.0,
-                                  right: 16.0
+                                    left:32.0,
+                                  right: 32.0
                                 ),
                                 child: const Text(
                                     "Save",
