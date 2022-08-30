94b64bebee4c1d8742e86fcdb7d265a604dd2a82
Aquarius-blake <55807270+Aquarius-blake@users.noreply.github.com>
2022-08-30 12:46:53 +0000

    Notifications

--- lib/Screens/Forum/Forum_detail.dart
+++ lib/Screens/Forum/Forum_detail.dart
@@ -13,6 +13,9 @@ class _ForumdetailState extends State<Forumdetail> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        iconTheme: IconThemeData(
+          color: Colors.black
+        ),
         title: Row(
           children: <Widget>[
 
