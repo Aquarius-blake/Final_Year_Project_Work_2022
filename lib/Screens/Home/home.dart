97f79fd921ce51ff997c8da9b396da7d8051a794
Aquarius-blake <55807270+Aquarius-blake@users.noreply.github.com>
2022-08-30 21:32:57 +0000

    Notifications

--- lib/Screens/Home/home.dart
+++ lib/Screens/Home/home.dart
@@ -87,7 +87,7 @@ class _HomeState extends State<Home> {
      );
     }
     catch(e){
-    return CircleAvatar(
+    return const CircleAvatar(
       backgroundImage: AssetImage('Assets/hac.jpg'),
       radius: 50,
     );
