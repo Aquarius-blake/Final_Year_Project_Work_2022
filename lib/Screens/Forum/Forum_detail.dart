32062fd7da24e6d10707f7957bbd761a88b6fcbc
Aquarius-blake <55807270+Aquarius-blake@users.noreply.github.com>
2022-08-30 21:17:37 +0000

    Notifications

--- lib/Screens/Forum/Forum_detail.dart
+++ lib/Screens/Forum/Forum_detail.dart
@@ -37,6 +37,7 @@ class _ForumdetailState extends State<Forumdetail> {
       body: ListView(
         children: [
           Container(),
+          Container()
         ],
       ),
       bottomNavigationBar: SafeArea(
