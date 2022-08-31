3de24da7f1fc73edb5039c4d741ea80879869f9e
Aquarius-blake <55807270+Aquarius-blake@users.noreply.github.com>
2022-08-31 19:26:33 +0000

    Notifications comment

--- lib/shared/Widgets/Comdetail.dart
+++ lib/shared/Widgets/Comdetail.dart
@@ -20,7 +20,7 @@ class _ComdetailState extends State<Comdetail> {
               .collection("Posts")
               .doc(widget.snap['Event Uid'])
               .collection("comments")
-             // .where('author uid',isEqualTo: widget.snap['author uid'])
+              .where('author uid',isEqualTo: widget.snap['author uid'])
               .orderBy("Comment Time",descending: true)
               .snapshots(),
           builder: (context,AsyncSnapshot<QuerySnapshot<Map<String,dynamic>>>snapshots){
