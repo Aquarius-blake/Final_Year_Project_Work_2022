888696a420aac1147c3755bfa5208ff478874dbe
Aquarius-blake <55807270+Aquarius-blake@users.noreply.github.com>
2022-08-31 18:37:23 +0000

    Notifications comment

--- lib/Services/Firestoremethods.dart
+++ lib/Services/Firestoremethods.dart
@@ -101,6 +101,10 @@ class FirestoreMethods{
                 String notifid=const Uuid().v1();
                 String message="Commented on your post";
                 Notifs notifs=Notifs(author_uid: author_uid, notifid: notifid, Eventuid: postid, message: message, author: author, Timeposted: DateTime.now(), owner_uid: owner_uid, title: title);
+                await _firestore.collection("Notification").doc(owner_uid).collection("Notifs").doc(notifid).set(
+                    notifs.toJson(),
+                    SetOptions(merge: true)
+                );
                 ress="Comment success";
                 return ress;
             }
