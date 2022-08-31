fb8576b7cab63a3f323657f7f59aac38b4fe9be4
Aquarius-blake <55807270+Aquarius-blake@users.noreply.github.com>
2022-08-31 11:30:12 +0000

    Notifications

---
+++ lib/shared/Widgets/Detailpost.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class detailp extends StatefulWidget {
+  const detailp({Key? key}) : super(key: key);
+
+  @override
+  State<detailp> createState() => _detailpState();
+}
+
+class _detailpState extends State<detailp> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
