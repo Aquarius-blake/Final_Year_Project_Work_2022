0a86a0299755d332fefac1de038c649e0427ec8f
Aquarius-blake <55807270+Aquarius-blake@users.noreply.github.com>
2022-08-31 19:07:01 +0000

    Notifications comment

--- lib/shared/Widgets/Comdetail.dart
+++ lib/shared/Widgets/Comdetail.dart
@@ -1,1 +1,15 @@
 import 'package:flutter/material.dart';
+
+class Comdetail extends StatefulWidget {
+  const Comdetail({Key? key}) : super(key: key);
+
+  @override
+  State<Comdetail> createState() => _ComdetailState();
+}
+
+class _ComdetailState extends State<Comdetail> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
