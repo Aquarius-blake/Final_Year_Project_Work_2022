7c2aaa34958e653ddf0165169779b7a8a6015e81
Aquarius-blake <55807270+Aquarius-blake@users.noreply.github.com>
2022-09-01 20:28:25 +0000

    message function

--- lib/shared/Widgets/Messagecard.dart
+++ lib/shared/Widgets/Messagecard.dart
@@ -15,75 +15,82 @@ class chatcard extends StatefulWidget {
 }
 
 class _chatcardState extends State<chatcard> {
- late bool recever;
+  late bool recever;
   @override
   Widget build(BuildContext context) {
     print(widget.snap['Message']);
     late  User1 user1=  Provider.of<UserProvider>(context).getUser;
-if(widget.snap['Receiver Uid']!=user1.UID){
-  recever=false;
-}else{
-  recever=true;
-}
+    if(widget.snap['Receiver Uid']!=user1.UID){
+      recever=false;
+    }else{
+      recever=true;
+    }
     if(recever){
-    return Container(
-      alignment: Alignment.centerRight,
-      child: Card(
-        color: Colors.lightBlueAccent,
-        elevation: 3.0,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            Text(widget.snap['Message']),
-            Align(
-              alignment: Alignment.bottomRight,
-                child: Text(
+      return Container(
+        alignment: Alignment.centerRight,
+        child: Card(
+          color: Colors.lightBlueAccent,
+          elevation: 3.0,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              RichText(
+                  text: TextSpan(
+                    text: widget.snap['Message'],
+                    style: TextStyle(
+                      color: Colors.white,
+                    ),
+                  )
+              ),
+              Align(
+                  alignment: Alignment.bottomRight,
+                  child: Text(
                     "Time",
-                  style: TextStyle(
-                    color: Colors.blueGrey,
-                    fontSize: 12
-                  ),
-                )
-            )
-          ],
+                    style: TextStyle(
+                        color: Colors.blueGrey,
+                        fontSize: 12
+                    ),
+                  )
+              )
+            ],
+          ),
         ),
-      ),
-    );
+      );
     }else{
       return Align(
         alignment: Alignment.bottomRight,
         child: Container(
           alignment: Alignment.bottomRight,
-         child: Card(
-           color: Colors.black,
-           elevation: 3.0,
-           child: Column(
-             crossAxisAlignment: CrossAxisAlignment.end,
-             mainAxisAlignment: MainAxisAlignment.end,
-             children: [
-               RichText(
-                   text: TextSpan(
-                     text: widget.snap['Message'],
-style: TextStyle(
-  color: Colors.white,
-),
-                   )
-               ),
+          child: Card(
+            color: Colors.black,
+            elevation: 3.0,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.end,
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                RichText(
+                    text: TextSpan(
+                      text: widget.snap['Message'],
+                      style: TextStyle(
+                        color: Colors.white,
+                      ),
+                    )
+                ),
 
-               Align(
-                 alignment: Alignment.bottomLeft,
-                   child: Text(
-                       "Time",
-                   style: TextStyle(
-                     color: Colors.blueGrey,
-                     fontSize: 12
-                   ),
-                   )
-               ),
-             ],
-           ),
-         ),
+                Align(
+                    alignment: Alignment.bottomLeft,
+                    child: Text(
+                      "Time",
+                      style: TextStyle(
+                          color: Colors.blueGrey,
+                          fontSize: 12
+                      ),
+                    )
+                ),
+              ],
+            ),
+          ),
         ),
       );
     }
