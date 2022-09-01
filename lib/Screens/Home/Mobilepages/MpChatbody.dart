9b1c766b5aa1efef0010dce915a49cbdfa66fe1c
Aquarius-blake <55807270+Aquarius-blake@users.noreply.github.com>
2022-09-01 19:21:44 +0000

    message function

---
+++ lib/Screens/Home/Mobilepages/MpChatbody.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Chatbody extends StatefulWidget {
+  const Chatbody({Key? key}) : super(key: key);
+
+  @override
+  State<Chatbody> createState() => _ChatbodyState();
+}
+
+class _ChatbodyState extends State<Chatbody> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
