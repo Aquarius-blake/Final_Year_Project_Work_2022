c9fdec59289b038de49bc017cd5c12c7b417d982
Aquarius-blake <55807270+Aquarius-blake@users.noreply.github.com>
2022-09-01 13:31:59 +0000

    Notifications comment

---
+++ lib/shared/Widgets/Messagecard.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class chatcard extends StatefulWidget {
+  const chatcard({Key? key}) : super(key: key);
+
+  @override
+  State<chatcard> createState() => _chatcardState();
+}
+
+class _chatcardState extends State<chatcard> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
