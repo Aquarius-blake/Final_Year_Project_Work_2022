09b01ff2c1429b689557e0d2ba33b2ae433fe26d
Aquarius-blake <55807270+Aquarius-blake@users.noreply.github.com>
2022-09-01 14:42:43 +0000

    Chat screen from profile

--- lib/Screens/Home/Mobilepages/MpchatScreen.dart
+++ lib/Screens/Home/Mobilepages/MpchatScreen.dart
@@ -25,7 +25,7 @@ class _MpchatScreenState extends State<MpchatScreen> {
             Text(
                 "Username",
             style: const TextStyle(
-
+color: Colors.black
             ),
             ),
           ],
