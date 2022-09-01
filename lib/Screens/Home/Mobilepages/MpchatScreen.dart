d019e0e82c0f1aca666ba926d1c7db0a47401abc
Aquarius-blake <55807270+Aquarius-blake@users.noreply.github.com>
2022-09-01 14:27:14 +0000

    chat cards

--- lib/Screens/Home/Mobilepages/MpchatScreen.dart
+++ lib/Screens/Home/Mobilepages/MpchatScreen.dart
@@ -1,1 +1,16 @@
 import 'package:flutter/material.dart';
+
+
+class MpchatScreen extends StatefulWidget {
+  const MpchatScreen({Key? key}) : super(key: key);
+
+  @override
+  State<MpchatScreen> createState() => _MpchatScreenState();
+}
+
+class _MpchatScreenState extends State<MpchatScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
