ed0484d9a819300c6eefdd747ebc8494bc8eb4f9
Aquarius-blake <55807270+Aquarius-blake@users.noreply.github.com>
2022-09-01 19:29:24 +0000

    message function

--- lib/Screens/Home/Mobilepages/MpChatbody.dart
+++ lib/Screens/Home/Mobilepages/MpChatbody.dart
@@ -1,7 +1,10 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:forum3/shared/Widgets/Messagecard.dart';
 
 class Chatbody extends StatefulWidget {
-  const Chatbody({Key? key}) : super(key: key);
+  final snap;
+  const Chatbody({Key? key,this.snap}) : super(key: key);
 
   @override
   State<Chatbody> createState() => _ChatbodyState();
@@ -10,6 +13,25 @@ class Chatbody extends StatefulWidget {
 class _ChatbodyState extends State<Chatbody> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body:  StreamBuilder(
+        stream: FirebaseFirestore.instance.collection('Chats').snapshots(),
+        builder: (context, AsyncSnapshot<QuerySnapshot<Map<String,dynamic>>>snapshot){
+          if(snapshot.connectionState==ConnectionState.waiting){
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+          return ListView.builder(
+              itemCount: snapshot.data!.docs.length,
+              itemBuilder: (context, index) => Container(
+                child: chatcard(
+                  snap: snapshot.data!.docs[index].data(),
+                ),
+              )
+          );
+        },
+      ),
+    );
   }
 }
