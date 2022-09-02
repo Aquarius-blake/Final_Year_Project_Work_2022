d6db083bffd0c3628a71217f182a998b082a37e2
Aquarius-blake <55807270+Aquarius-blake@users.noreply.github.com>
2022-09-02 19:29:51 +0000

    Delete message feature

--- lib/Screens/Home/Mobilepages/MmChatBody.dart
+++ lib/Screens/Home/Mobilepages/MmChatBody.dart
@@ -87,7 +87,7 @@ class _mChatbodyState extends State<mChatbody> {
                         child: GestureDetector(
                           onLongPress: (){
                             if(snapshot.data!.docs[index].data()['author uid']==user1.UID){
-
+_options(context);
                             }else{
                               Showsnackbar("Access Denied", context);
                             }
