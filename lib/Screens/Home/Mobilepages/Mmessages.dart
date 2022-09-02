2bb3a4f5d9b01cd18135111980a40c14ee7dc760
Aquarius-blake <55807270+Aquarius-blake@users.noreply.github.com>
2022-09-02 08:56:47 +0000

    chat head ui

--- lib/Screens/Home/Mobilepages/Mmessages.dart
+++ lib/Screens/Home/Mobilepages/Mmessages.dart
@@ -1,5 +1,11 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:forum3/shared/Widgets/Chatheadcard.dart';
+import 'package:provider/provider.dart';
+
+import '../../../Models/Users1.dart';
+import '../../../Provider/user_provider.dart';
 
 class Mmessages extends StatefulWidget {
   const Mmessages({Key? key}) : super(key: key);
@@ -11,11 +17,23 @@ class Mmessages extends StatefulWidget {
 class _MmessagesState extends State<Mmessages> {
   @override
   Widget build(BuildContext context) {
+    late  User1 user1=  Provider.of<UserProvider>(context).getUser;
     return Scaffold(
-body: SafeArea(
-    child: Container(
-
-    )
+body: StreamBuilder(
+  stream: FirebaseFirestore.instance.collection('Chats').doc(user1.UID).collection("Chathead").orderBy("Chat Time").snapshots(),
+  builder: (context, AsyncSnapshot<QuerySnapshot<Map<String,dynamic>>>snapshot){
+    if(snapshot.connectionState==ConnectionState.waiting){
+      return const Center(
+        child: CircularProgressIndicator(),
+      );
+    }
+    return ListView.builder(
+        itemCount: snapshot.data!.docs.length,
+        itemBuilder: (context, index) => CHcard(
+          snap: snapshot.data!.docs[index].data(),
+        )
+    );
+  },
 ),
       floatingActionButton: FloatingActionButton(
         onPressed: (){},
