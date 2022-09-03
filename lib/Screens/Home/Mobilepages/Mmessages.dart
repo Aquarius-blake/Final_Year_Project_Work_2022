3b399774c6114717ef222c6e94c0cad8b206e6b5
Aquarius-blake <55807270+Aquarius-blake@users.noreply.github.com>
2022-09-03 16:08:38 +0000

    Sign in as guest bug fixes

--- lib/Screens/Home/Mobilepages/Mmessages.dart
+++ lib/Screens/Home/Mobilepages/Mmessages.dart
@@ -20,7 +20,8 @@ class _MmessagesState extends State<Mmessages> {
   @override
   Widget build(BuildContext context) {
     late  User1 user1=  Provider.of<UserProvider>(context).getUser;
-    return Scaffold(
+
+    return user1.Guest==true? Scaffold(): Scaffold(
 body: StreamBuilder(
   stream: FirebaseFirestore.instance.collection('Chats').doc(user1.UID).collection("Chathead").orderBy("Chat Time",descending: true).snapshots(),
   builder: (context, AsyncSnapshot<QuerySnapshot<Map<String,dynamic>>>snapshot){
