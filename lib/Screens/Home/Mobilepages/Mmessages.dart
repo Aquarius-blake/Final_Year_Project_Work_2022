fa8aeb71009da226b2067e6fce8205ae0718453b
Aquarius-blake <55807270+Aquarius-blake@users.noreply.github.com>
2022-09-03 16:15:01 +0000

    Sign in as guest bug fixes

--- lib/Screens/Home/Mobilepages/Mmessages.dart
+++ lib/Screens/Home/Mobilepages/Mmessages.dart
@@ -21,16 +21,23 @@ class _MmessagesState extends State<Mmessages> {
   Widget build(BuildContext context) {
     late  User1 user1=  Provider.of<UserProvider>(context).getUser;
 
-    return user1.Guest==true? const Scaffold(
+    return user1.Guest==true?  Scaffold(
       body: SafeArea(
         child: Center(
-child: Text(
-    "Access Denied",
-style: TextStyle(
-  fontSize: 32,
-  fontWeight: FontWeight.bold,
-),
-),
+child:RichText(
+    text: TextSpan(
+      children: [
+        TextSpan(
+          text: "Access Denied",
+          style: TextStyle(
+            fontSize: 32,
+            fontWeight: FontWeight.bold
+          )
+
+        ),
+      ],
+    )
+)
         ),
       ),
     ): Scaffold(
