7bbbe8f6b533d2d3cd402900a063b01a8c01816b
Aquarius-blake <55807270+Aquarius-blake@users.noreply.github.com>
2022-09-04 17:55:30 +0000

    Share Feature + Dynamic links dependencies + some touch ups

--- lib/Screens/Home/home.dart
+++ lib/Screens/Home/home.dart
@@ -1,6 +1,7 @@
 
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_dynamic_links/firebase_dynamic_links.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
@@ -38,7 +39,9 @@ class _HomeState extends State<Home> {
 
 
 
-  void initiateDynamiclink()async{}
+  void initiateDynamiclink()async{
+    FirebaseDynamicLinks.instance.onLink;
+  }
 
 
 
