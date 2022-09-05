61f872955fae43193c80def21cc5ceb60235a88c
Aquarius-blake <55807270+Aquarius-blake@users.noreply.github.com>
2022-09-05 13:01:59 +0000

    refactor

--- lib/Screens/Home/Mobilepages/Meditprofile.dart
+++ lib/Screens/Home/Mobilepages/Meditprofile.dart
@@ -240,8 +240,7 @@ late DateTime? _dateTime;
                             ),
                           ),
 
-                          style: TextStyle(
-                          ),
+                          style: const TextStyle(),
                         ),
                         const SizedBox(height: 10,),
                         ListTile(
