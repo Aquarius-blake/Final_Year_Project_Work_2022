814da303c0b1837d1e0b7ff4704450616c05f1c5
Aquarius-blake <55807270+Aquarius-blake@users.noreply.github.com>
2022-09-07 20:21:33 +0000

    bug

--- lib/Screens/Platforms/WebSceens/Web_editProfile.dart
+++ lib/Screens/Platforms/WebSceens/Web_editProfile.dart
@@ -106,7 +106,7 @@ class _WedProfileState extends State<WedProfile> {
 
   @override
   Widget build(BuildContext context) {
-    //TODO:Fix Bug
+    //TODO:Fix Bug(Error) before running
     return Scaffold(
       backgroundColor: Colors.lightBlueAccent,
       appBar: AppBar(
