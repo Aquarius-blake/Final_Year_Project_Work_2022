52574a09ea55d0bb1bc8005b6bd9a29988e124bf
Aquarius-blake <55807270+Aquarius-blake@users.noreply.github.com>
2022-09-09 22:56:45 +0000

    Admin touch ups

--- lib/Screens/Home/Mobilepages/Mrequest_page.dart
+++ lib/Screens/Home/Mobilepages/Mrequest_page.dart
@@ -1,1 +1,17 @@
 import 'package:flutter/material.dart';
+
+class Request_page extends StatefulWidget {
+  const Request_page({Key? key}) : super(key: key);
+
+  @override
+  State<Request_page> createState() => _Request_pageState();
+}
+
+class _Request_pageState extends State<Request_page> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
