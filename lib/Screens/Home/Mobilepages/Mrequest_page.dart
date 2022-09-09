2fc973429c2f1d89ff196ac327870354477cb231
Aquarius-blake <55807270+Aquarius-blake@users.noreply.github.com>
2022-09-09 23:04:39 +0000

    Admin touch ups

--- lib/Screens/Home/Mobilepages/Mrequest_page.dart
+++ lib/Screens/Home/Mobilepages/Mrequest_page.dart
@@ -1,4 +1,6 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:forum3/shared/Widgets/Request_card.dart';
 
 class Request_page extends StatefulWidget {
   const Request_page({Key? key}) : super(key: key);
@@ -24,6 +26,24 @@ appBar: AppBar(
     ),
   ),
 ),
+      body: StreamBuilder(
+        stream: FirebaseFirestore.instance.collection('Requests').snapshots(),
+        builder: (context, AsyncSnapshot<QuerySnapshot<Map<String,dynamic>>>snapshot){
+          if(snapshot.connectionState==ConnectionState.waiting){
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+          return ListView.builder(
+              itemCount: snapshot.data!.docs.length,
+              itemBuilder: (context, index) => Container(
+                child: RequestCard(
+                  snap: snapshot.data!.docs[index].data(),
+                ),
+              )
+          );
+        },
+      ),
     );
   }
 }
