15d7eca3fc764a4c4c428a371a78375361d3c3f0
Aquarius-blake <55807270+Aquarius-blake@users.noreply.github.com>
2022-09-09 19:47:07 +0000

    Admin touch ups

---
+++ lib/shared/Widgets/Request_card.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class RequestCard extends StatefulWidget {
+  final snap;
+  const RequestCard({Key? key,this.snap}) : super(key: key);
+
+  @override
+  State<RequestCard> createState() => _RequestCardState();
+}
+
+class _RequestCardState extends State<RequestCard> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
